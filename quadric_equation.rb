f771c17e8e498dbda5cad1b97619208b07570484
Ilya <burinskiy992@gmail/com>
2020-09-29 19:48:29 +0300

    added main func in quadric equation

--- quadric_equation.rb
+++ quadric_equation.rb
@@ -1,22 +1,26 @@
-print "a: "
-a = gets.to_i
+def main
+    print "a: "
+    a = gets.to_i
 
-print "b: "
-b = gets.to_i
+    print "b: "
+    b = gets.to_i
 
-print "c: "
-c = gets.to_i
+    print "c: "
+    c = gets.to_i
 
-D = b ** 2 - 4 * a * c
+    d = b ** 2 - 4 * a * c
 
-puts "D = #{D}"
-if D > 0
-    root1 = (-b + (D ** 0.5)) / 2 * a
-    root2 = (-b - (D ** 0.5)) / 2 * a
-    puts "roots: #{root1}, #{root2}"
-elsif D == 0
-    root = -b / 2 / a
-    puts "root #{root}"
-else
-    puts "imaginary roots"
+    puts "D = #{d}"
+    if d > 0
+        root1 = (-b + (d ** 0.5)) / 2 * a
+        root2 = (-b - (d ** 0.5)) / 2 * a
+        puts "roots: #{root1}, #{root2}"
+    elsif d == 0
+        root = -b / 2 / a
+        puts "root #{root}"
+    else
+        puts "imaginary roots"
+    end
 end
+
+main
