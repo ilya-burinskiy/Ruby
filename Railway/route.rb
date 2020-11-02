2c707cfeb3aba18579706cee74d6e5b75f71297a
Ilya <burinskiy992@gmail/com>
2020-11-02 16:41:10 +0300

    route iterator

--- Railway/route.rb
+++ Railway/route.rb
@@ -1,5 +1,27 @@
 class Route
 
+  class RouteIterator
+
+      attr_reader :stations
+      attr_reader :cursor
+
+      attr_writer :cursor
+
+      def initialize(route, cursor)
+          @stations = [route.dispatch_station, 
+                      *route.intermediate_stations,
+                      route.terminal_station]
+          @cursor = cursor
+      end
+
+      def next()
+        if self.cursor + 1 >= self.stations.length
+          raise Exception.new
+        self. cursor += 1
+        return self.stations[cursor]
+      end
+  end
+
   attr_reader :intermediate_stations
   attr_reader :dispatch_station
   attr_reader :terminal_station
@@ -19,12 +41,15 @@ class Route
   end
 
   def print_route()
-    puts self.dispatch_station
+    puts self.dispatch_station.name
     self.intermediate_stations.each do |station|
-      puts station
+      puts station.name
     end
-    puts self.terminal_station
+    puts self.terminal_station.name
   end
 
+  def get_iterator()
+    return RouteIterator(self, -1)
+  end
 
 end
