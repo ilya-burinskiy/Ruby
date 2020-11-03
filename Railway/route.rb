56fc419ff00e029e2950194d80cba77e91e12a5f
Ilya <burinskiy992@gmail/com>
2020-11-03 14:43:49 +0300

    deleted class RouteIterator
    The RouteIterator was removed, instead Route objects can return the previous
    and next station for some station

--- Railway/route.rb
+++ Railway/route.rb
@@ -1,10 +1,6 @@
-load 'route_iterator.rb'
-
 class Route
 
-  attr_reader :intermediate_stations
-  attr_reader :dispatch_station
-  attr_reader :terminal_station
+  attr_reader :intermediate_stations, :dispatch_station, :terminal_station
 
   def initialize(dispatch_station, terminal_station)
     @dispatch_station = dispatch_station
@@ -13,23 +9,31 @@ class Route
   end
 
   def add_station(station)
-    self.intermediate_stations << station
+    intermediate_stations << station
   end
 
   def remove_station(station)
-    self.intermediate_stations.delete(station)
+    intermediate_stations.delete(station)
   end
 
-  def print_route()
-    puts self.dispatch_station.name
-    self.intermediate_stations.each do |station|
+  def print_route
+    puts dispatch_station.name
+    intermediate_stations.each do |station|
       puts station.name
     end
-    puts self.terminal_station.name
+    puts terminal_station.name
   end
 
-  def get_iterator()
-    return RouteIterator.new(self)
+  def station_next_to(station)
+    stations = [dispatch_station, *intermediate_stations, terminal_station]
+    station_idx = stations.index(station)
+    stations.fetch(station_idx + 1, nil)
+  end
+
+  def station_previous_to(station)
+    stations = [dispatch_station, *intermediate_stations, terminal_station]
+    station_idx = stations.index(station)
+    stations.fetch(station_idx - 1) if station_idx - 1 >= 0
   end
 
 end
