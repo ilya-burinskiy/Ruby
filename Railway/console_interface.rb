9e3d00325d93fd19e75dfa585bd6ee23687cf5a8
Ilya <burinskiy992@gmail/com>
2020-11-07 12:30:41 +0300

    console interface

---
+++ Railway/console_interface.rb
@@ -0,0 +1,24 @@
+class ConsoleInterface
+
+  def read_station_name
+    print "Enter station name: "
+    station_name = gets.chomp
+  end
+
+  def read_train_number
+    print "Enter train number: "
+    train_number = gets.chomp
+  end
+
+  def read_train_type
+    print "Enter train type: "
+    train_type = gets.chomp.downcase
+  end
+
+  def read_route_name
+    print "Enter route name: "
+    route_name = gets.chomp
+  end
+
+  def read_route
+end
