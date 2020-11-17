1eafdc02fecd4726d659818e8cf6e76ed2804f51
Ilya <burinskiy992@gmail/com>
2020-11-17 12:32:46 +0300

    Validation methods are created dynamically

--- Railway/validation.rb
+++ Railway/validation.rb
@@ -5,13 +5,58 @@ module Validation
   end
 
   module ClassMethods
+
     def validate(name, vtype, vparam = nil)
+
       @vars_validators ||= {}
       @vars_validators[name] ||= []
       @vars_validators[name] << { vtype: vtype, vparam: vparam }
+
+      @is_vmethod_defined ||= {:precence => false, :format => false, :type => false}
+
+      unless @is_vmethod_defined[vtype]
+        case vtype
+        when :precence
+          define_precence_vmethod(vtype)
+        when :format
+          definde_format_vmethod(vtype)
+        when :type
+          define_type_vmethod(vtype)
+        else
+          raise 'Unknown validator type'
+        end
+        @is_vmethod_defined[vtype] = true
+      end
     end
 
     attr_reader :vars_validators
+
+    private
+
+    def define_precence_vmethod(vtype)
+      define_method("validate_#{vtype}".to_sym) do |var_name, vparam|
+        if instance_variable_get("@#{var_name}".to_sym).nil?
+          raise "@#{var_name} must have non nil value" 
+        end
+      end
+    end
+
+    def definde_format_vmethod(vtype)
+      define_method("validate_#{vtype}".to_sym) do |var_name, format|
+        if instance_variable_get("@#{var_name}".to_sym) !~ format
+          raise "@#{var_name} does not match the format" 
+        end
+      end
+    end
+
+    def define_type_vmethod(vtype)
+      define_method("validate_#{vtype}".to_sym) do |var_name, cls|
+        if instance_variable_get("@#{var_name}".to_sym).class != cls 
+          raise "@#{var_name} does not match the class"
+        end
+      end
+    end
+
   end
 
   module InstanceMethods
@@ -20,16 +65,7 @@ module Validation
         validators_params_list.each do |validator_params|
           vtype = validator_params[:vtype]
           vparam = validator_params[:vparam]
-          case vtype
-          when :precence
-            validate_precence(var_name)
-          when :format
-            validate_format(var_name, vparam)
-          when :type
-            validate_type(var_name, vparam)
-          else
-            raise 'Unknown validator type'
-          end
+          send("validate_#{vtype}", var_name, vparam)
         end
       end
     end
@@ -40,22 +76,7 @@ module Validation
     rescue
       false
     end
-
-    private
-
-    def validate_precence(var_name)
-      raise "@#{var_name} must have non nil value" if instance_variable_get("@#{var_name}".to_sym).nil?
-    end
-
-    def validate_format(var_name, format)
-      raise "@#{var_name} does not match the format" if instance_variable_get("@#{var_name}".to_sym) !~ format 
-    end
-
-    def validate_type(var_name, cls)
-      if instance_variable_get("@#{var_name}".to_sym).class != cls 
-        raise "@#{var_name} does not match the class"
-      end
-    end
   
   end
+  
 end
